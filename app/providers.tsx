276f9f53dda1c02821c1469201e0976af950abca
Dhanush S <143257334+DhanushSelvarajDev@users.noreply.github.com>
2026-02-01 00:24:07 +0530

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,9 +2,16 @@
 
 import React from "react"
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from '@/theme';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={theme} resetCSS>
+        {children}
+      </ChakraProvider>
+    </>
+  );
 }
